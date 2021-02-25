528849bb72b4514aa58269652e9b24de1f2dcbbf
Luiz Gustavo Banin De Sena <luizgbs1@gmail.com>
2021-02-25 18:45:19 -0300

    Class Cliente

--- SysBil/ModuloVendas/Program.cs
+++ SysBil/ModuloVendas/Program.cs
@@ -10,6 +10,16 @@ namespace ModuloVendas
     {
         static void Main(string[] args)
         {
+            string[] linhaDigital = new string[100];
+            linhaDigital[0] = "Jaqueline Mistron"; // 25 caracteres
+            linhaDigital[50] = "";
+            StringBuilder teste = new StringBuilder();
+            string nome50Pos = "Jaqueline Mistron".PadRight(50, ' ');
+            string resultado = string.Concat(nome50Pos);
+            Console.WriteLine("{0}", nome50Pos);
+            //teste.Insert(0, nome25Pos);
+            //teste.Insert(25, "123456789");
+            Console.WriteLine(resultado.ToString());
         }
     }
 }
