e89f5430f62a7c65ba035fccd1e42ebae6d3a1bc
Pes <Pes@SITNOTT01>
2021-02-26 10:38:54 -0300

    Integração do projeto

--- SysBil/SysBil/Venda.cs
+++ SysBil/SysBil/Venda.cs
@@ -4,19 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SysBil
+namespace ModuloVendas
 {
     class Venda
-
-        //Propriedades
-    {        
+    {
         public int Id { get; set; }
 
         public DateTime Dcompra { get; set; }
-    
-        public string  Produto { get; set; }
 
-        public int Qtd {get;set;}
+        public string Produto { get; set; }
+
+        public int Qtd { get; set; }
 
         public double Vunitario { get; set; }
 
@@ -26,47 +24,38 @@ namespace SysBil
 
         public void Cadastrar()
         {
-                    
+
         }
 
         public void Localizar()
         {
-                    
+
         }
 
         public void Editar()
         {
-                    
+
         }
 
         public void Excluir()
         {
-                    
+
         }
 
         public void ImpressaoPorRegistro()
         {
-                    
+
         }
 
         public void Inadimplente()
         {
-        
+
         }
 
         public void LimiteProduto()
         {
 
         }
-
-
-
-
-
-
-
-
-
 
 
     }
