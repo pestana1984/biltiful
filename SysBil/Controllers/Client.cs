66fb3b1ab4568b96e7d255926cb4239906deec04
Luiz Gustavo Banin De Sena <luizgbs1@gmail.com>
2021-02-26 22:45:11 -0300

    ações de cliente

--- SysBil/Controllers/Client.cs
+++ SysBil/Controllers/Client.cs
@@ -11,9 +11,10 @@ namespace Controllers
 {
     public class Client
     {
-        public static string GetClient(Cliente c)
+        // RETORNO DE DADOS DE CLIENTE
+        public static string GetClient(Cliente c) 
         {
-            if(c.Situacao == 'A')
+            if(c.Situacao == 'A') // SE O CLIENTE FOR ATIVO RETORNA
                 return "\n>>>Cliente " + c.Nome + "<<<" +
                     "\nCPF: " + c.Cpf +
                     "\nData Nascimento: " + c.DNascimento.ToString("dd/MM/yyyy") +
@@ -22,25 +23,33 @@ namespace Controllers
                     "\nData de Cadastro: " + c.DCadastro.ToString("dd/MM/yyyy") + "\n";
             return "";
         }
-        public static void WriteFile(List<Cliente> listaCliente)
+
+        // ESCRITA DE ARQUIVO
+        public static void WriteFile(List<Cliente> listaCliente) 
         {
             using (StreamWriter file = new StreamWriter(@"C:\Users\Luiz Sena\source\repos\LuizGustavoSena\biltiful\biltiful\SysBil\Clientes.dat"))
             {
                 foreach (Cliente c in listaCliente)
-                    file.WriteLine(GetClientFile(c));
+                    file.WriteLine(GetClientFile(c)); // ESCREVE A LISTA NO ARQUIVO SEPARADOS POR QUEBRA LINHA
             }
         }
-        public static void ReadFile(List<Cliente> listaCliente)
+
+        // LEITURA DE ARQUIVO
+        public static void ReadFile(List<Cliente> listaCliente) 
         {
+            // SE O ARQUIVO EXISTIR
             if(File.Exists(@"C:\Users\Luiz Sena\source\repos\LuizGustavoSena\biltiful\biltiful\SysBil\Clientes.dat"))
             {
                 using(StreamReader file = new StreamReader(@"C:\Users\Luiz Sena\source\repos\LuizGustavoSena\biltiful\biltiful\SysBil\Clientes.dat"))
                 {
+                    // VARIAVEIS
                     string nome, cpf, nasc, uCom, dCad ;
                     char sexo, situacao;
 
+                    // ENQUANTO ARQUIVO EXISTIR
                     while (!file.EndOfStream)
                     {
+                        // INICIALIZACAO DE VARIAVEIS
                         nasc = "";
                         uCom = "";
                         dCad = "";
@@ -49,19 +58,19 @@ namespace Controllers
                         sexo = ' ';
                         situacao = ' ';
 
-                        char[] line = file.ReadLine().ToCharArray();
+                        char[] line = file.ReadLine().ToCharArray(); // ARMAZENA A LINHA EM CARACTERES
 
-                        for (int i = 0; i < 11; i++) // CPF
+                        for (int i = 0; i < 11; i++) // LE CPF
                             cpf += line[i];
 
-                        for (int i = 11; i < 61 && line[i] != ' ' ; i++) // NOME
+                        for (int i = 11; i < 61 && line[i] != ' ' ; i++) // LE NOME
                         {
                             if (line[i] == '-')
                                 line[i] = ' ';
                             nome += line[i];
                         }
 
-                        // DATA NASCIMENTO
+                        // LE DATA NASCIMENTO
                         for (int i = 64; i < 67; i++) // MM/
                             nasc += line[i];
                         for (int i = 61; i < 64; i++) // MM/dd/
@@ -69,9 +78,9 @@ namespace Controllers
                         for (int i = 67; i < 71; i++) // MM/dd/yyyy
                             nasc += line[i];
 
-                        sexo = line[71]; // SEXO
+                        sexo = line[71]; // LE SEXO
 
-                        // DATA ULTIMA COMPRA
+                        // LE DATA ULTIMA COMPRA
                         for (int i = 75; i < 78; i++) // MM/
                             uCom += line[i];
                         for (int i = 72; i < 75; i++) // MM/dd/
@@ -79,7 +88,7 @@ namespace Controllers
                         for (int i = 78; i < 82; i++) // MM/dd/yyyy
                             uCom += line[i];
 
-                        // DATA CADASTRO
+                        // LE DATA CADASTRO
                         for (int i = 85; i < 88; i++) // MM/
                             dCad += line[i];
                         for (int i = 82; i < 85; i++) // MM/dd/
@@ -87,9 +96,9 @@ namespace Controllers
                         for (int i = 88; i < 92; i++) // MM/dd/yyyy
                             dCad += line[i];
 
-                        situacao = line[92];
+                        situacao = line[92]; // LE SITUACAO
 
-
+                        //ADICIONANDO CLIENTE A LISTA
                         listaCliente.Add(new Cliente()
                         {
                             Cpf = cpf,
@@ -104,18 +113,42 @@ namespace Controllers
                 }
             }
         }
+
+        // VERIFICA CPF REPETE
+        public static bool CpfRepeat(List<Cliente> lista, string cpf)
+        {
+            Console.WriteLine("Entrou");
+            foreach(Cliente i in lista)
+            {
+                if(i.Cpf.Equals(cpf))
+                    return true;
+            }
+            return false;
+        }
+
+        // RETORNA CLIENTE NO FORMATO PARA ARQUIVO
         private static string GetClientFile(Cliente c)
         {
             return c.Cpf + NameToFile(c.Nome) + c.DNascimento.ToString("dd/MM/yyyy") +
                 c.Sexo + c.UCompra.ToString("dd/MM/yyyy") + c.DCadastro.ToString("dd/MM/yyyy") + c.Situacao;
         }
+
+        // PROCURA CLIENTE
         public static Cliente SearchClient(List<Cliente> c, string value)
         {
             foreach(Cliente i in c)
-                if(i.Nome == value)
+                if(i.Nome == value) 
                     return i;
             return null;
         }
+
+        // DELETA CLIENTE LOGICAMENTE
+        public static void DeleteClient(Cliente c)
+        {
+            c.Situacao = 'I';
+        }
+
+        // VERIFICA IDADE (MAIOR OU MENOR DE 18 ANOS)
         public static bool CheckAge(DateTime value)
         {
             var birthdate = value;
@@ -126,25 +159,29 @@ namespace Controllers
                 return true;
             return false;
         }
+
+        // TRATAMENTO DO NOME PARA ENVIAR AO ARQUIVO
         private static string NameToFile(string value)
         {
             char[] caracter = value.ToCharArray();
             int amo = value.Length;
             string name = "" ;
 
-            for (int i = 0; i < amo; i++)
+            for (int i = 0; i < amo; i++) // TROCA O ESPACO PARA UMA '-'
             {
                 if (caracter[i] == ' ')
                     caracter[i] = '-';
                 name += caracter[i];
             }
 
-            for (; amo < 50; amo++)
+            for (; amo < 50; amo++) // ADICIONA OS ESPAÇOS ATÉ O LIMITE DA VARIAVEL
             {
                 name += ' ';
             }
             return name;
         }
+
+        // VERIFICA SE O CPF É VERDADEIRO
         public static bool CpfIsValid(string value)
         {
             int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
--- SysBil/SysBil/Program.cs
+++ SysBil/SysBil/Program.cs
@@ -12,39 +12,79 @@ namespace SysBil
 	{
 		static void Main(string[] args)
 		{
+			// VARIAVEIS
 			Cliente c;
 			List<Cliente> listaCliente = new List<Cliente>();
 			byte op;
 			string buscar;
-			Client.ReadFile(listaCliente);
+
+			Client.ReadFile(listaCliente); // LE OS CLIENTES DO ARQUIVO
 
             do
-            {
-				op = Menu();
+            { // LAÇO DE OPÇOES
 
-				Console.Clear();
+				op = Menu(); // CHAMA MENU
+
+				Console.Clear(); // LIMPA A TELA
 
 				switch (op)
                 {
 					case 1: // INSERIR
 
-						c = lerCliente();
-						listaCliente.Add(c);
-						listaCliente = listaCliente.OrderBy(x => x.Nome).ToList();
-						Client.WriteFile(listaCliente);
+						c = lerCliente(listaCliente); // LE O CLIENTE
+
+						listaCliente.Add(c); // ADICIONA CLIENTE A LISTA
+
+						listaCliente = listaCliente.OrderBy(x => x.Nome).ToList(); // ORDENA A LISTA POR NOME
+
+						Client.WriteFile(listaCliente); // ADICIONA A LISTA NO ARQUIVO
+
+						Console.WriteLine("Cliente Cadastrado");
 
 						break;
 					case 2: // IMPRIMIR
+
 						listaCliente.ForEach(x => Console.WriteLine(Client.GetClient(x)));
+
 						break;
-					case 3: // BUSCAR
-						Console.Write("Informe o Cliente que deseja buscar: ");
-						buscar = Console.ReadLine();
-						c = Client.SearchClient(listaCliente, buscar);
-						if (c != null)
+					case 3: // LOCALIZAR
+
+						Console.Write("Informe o Nome do Cliente que deseja buscar: ");
+						buscar = Console.ReadLine(); // LE O NOME DO CLIENTE A SER BUSCADO
+
+						c = Client.SearchClient(listaCliente, buscar); // LOCALIZA CLIENTE
+
+						if (c != null && c.Situacao == 'A') // SE CLIENTE EXISTIR E TIVER SITUAÇÃO ATIVA
 							Console.WriteLine(Client.GetClient(c));
 						else
 							Console.WriteLine("Cliente não encontrado");
+						break;
+					case 4: // ATUALIZAR CLIENTE
+
+						Console.Write("Informe o Nome do Cliente que deseja Alterar: ");
+						buscar = Console.ReadLine(); // LE O NOME DO CLIENTE A SER BUSCADO
+
+						c = Client.SearchClient(listaCliente, buscar); // LOCALIZA CLIENTE
+
+						atualizaCliente(c, listaCliente); // ATUALIZA O CAMPO QUE O CLIENTE DESEJA DELE
+
+						Client.WriteFile(listaCliente); // ADICIONA A LISTA NO ARQUIVO
+
+						Console.WriteLine("Cliente Atualizado");
+
+						break;
+					case 5: // DELETAR LOGICAMENTE
+
+						Console.Write("Informe o Nome do Cliente que deseja Alterar: ");
+						buscar = Console.ReadLine(); // LE O NOME DO CLIENTE A SER BUSCADO
+						c = Client.SearchClient(listaCliente, buscar);
+
+						Client.DeleteClient(c); // DELETA O CLIENTE LOGICAMENTE
+
+						Client.WriteFile(listaCliente); // ADICIONA A LISTA NO ARQUIVO
+
+						Console.WriteLine("Cliente Deletado logicamente");
+
 						break;
                 }
             } while (op != 0);
@@ -52,50 +92,42 @@ namespace SysBil
 
 		private static byte Menu()
         {
-			string opcao;
+			// VARIAVEIS
+			string opcao; 
 			byte op;
+
+						// MENU
 			Console.WriteLine("\n------->>> MENU <<<-------");
 			Console.WriteLine("\n1 - Inserir Cliente" +
 								"\n2 - Imprimir Clientes" +
 								"\n3 - Buscar Cliente" +
+								"\n4 - Atualizar Cliente" +
+								"\n5 - Deletar Cliente" +
 								"\n0 - Sair" +
 								"\n\n--------------------------");
 			opcao = Console.ReadLine();
-			if (byte.TryParse(opcao, out op))
+
+			if (byte.TryParse(opcao, out op)) // SE CONSEGUIR CONVERTER PARA BYTE IMPRIME
 				return op;
-			return Menu();
+			return Menu(); // SE NÃO CONSEGUIR CHAMA A FUNÇÃO NOVAMENTE
         }
 
-		private static Cliente lerCliente()
+		private static Cliente lerCliente(List<Cliente> lista)
         {
+			// VARIAVEIS
 			string cpf, nome;
 			char sexo;
 			DateTime dNascimento;
 
-			do {
-				Console.Write("Informe o CPF: ");
-				cpf = Console.ReadLine();
-			} while (!Client.CpfIsValid(cpf));
+			cpf = lerCpf(lista); // VERIFICA CPF VALIDO E SE REPETE
 
-            do
-            {
-				Console.Write("Informe o Nome: ");
-				nome = Console.ReadLine();
-            } while (nome == "");
+			nome = lerNome(); // LE O NOME 
 
-            do
-            {
-				Console.Write("Infome o Sexo M - Masculino F - Feminino: ");
-				sexo = char.Parse(Console.ReadLine().ToUpper());
-            } while (sexo != 'M' && sexo != 'F');
+			sexo = lerSexo(); // LE O CARACTER DE SEXO
 
-            do
-            {
-				Console.Write("Informe sua Data de Nascimento: ");
-				dNascimento = DateTime.Parse(Console.ReadLine());
-			} while (!Client.CheckAge(dNascimento));
+			dNascimento = lerDNascimento(); // LE A DATA DE NASCIMENTO
 
-			return new Cliente()
+			return new Cliente() // RETORNA O CLIENTE
 			{
 				Cpf = cpf,
 				Nome = nome,
@@ -105,6 +137,77 @@ namespace SysBil
 				Sexo = sexo,
 				Situacao = 'A'
 			};
+		}
+		private static void atualizaCliente(Cliente c, List<Cliente> lista)
+        {
+			byte att;
+
+			Console.WriteLine("Qual campo deseja atualizar:\n1 - CPF\n2 - Nome\n3 - Sexo");
+			att = byte.Parse(Console.ReadLine());
+
+			switch (att)
+            {
+				case 1:
+					c.Cpf = lerCpf(lista); // ATUALIZA SOMENTE O CPF
+					break;
+				case 2:
+					c.Nome = lerNome(); // ATUALIZA SOMENTE NOME
+					break;
+				case 3:
+					c.Sexo = lerSexo(); // ATUALIZA SOMENTE SEXO
+					break;
+			}
+        }
+
+		private static string lerCpf(List<Cliente> lista)
+        {
+			string cpf;
+
+			do
+			{ // LAÇO VERIFICA CPF SE É VALIDO E SE JÁ EXISTE NA LISTA DE CLIENTES
+				Console.Write("Informe o CPF: ");
+				cpf = Console.ReadLine();
+			} while (!(Client.CpfIsValid(cpf)) && !(Client.CpfRepeat(lista, cpf)));
+			       // VERIFICA SE CPF É VALIDO      VERIFICA SE CPF JÁ EXISTE NA LISTA
+
+			return cpf;
+		}
+		private static string lerNome()
+        {
+			string nome;
+
+			do
+			{ // LAÇO PARA NÃO DEIXAR CAMPO VAZIO
+				Console.Write("Informe o Nome: ");
+				nome = Console.ReadLine();
+			} while (nome == "");
+
+			return nome;
+		}
+
+		private static char lerSexo()
+        {
+			char sexo;
+
+			do
+			{ // LAÇO VERIFICA SE O CAMPO É DIFERENTE DO SOLICITADO
+				Console.Write("Infome o Sexo M - Masculino F - Feminino: ");
+				sexo = char.Parse(Console.ReadLine().ToUpper());
+			} while (sexo != 'M' && sexo != 'F');
+
+			return sexo;
+		}
+
+		private static DateTime lerDNascimento()
+        {
+			DateTime dNascimento;
+
+			do
+			{ // LAÇO VERIFICA A IDADE COM A DATA DE NASCIMENTO INFORMADA
+				Console.Write("Informe sua Data de Nascimento: ");
+				dNascimento = DateTime.Parse(Console.ReadLine());
+			} while (!Client.CheckAge(dNascimento));
+			return dNascimento;
 		}
 	}
 }
