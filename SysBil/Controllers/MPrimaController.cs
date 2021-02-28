1ca9fc4aa0a6ec31a38cc0fd306c8d8b02d4ef9e
saulogp <sauloproetti@gmail.com>
2021-02-28 14:07:43 -0300

    cadastrar materia prima & classe materia prima

---
+++ SysBil/Controllers/MPrimaController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class MPrimaController
+    {
+        public static MPrima Cadastrar()
+        {
+
+            //Gerador de id para matéria prima
+            //MP1234 -> formato | guardando resultados
+            int[] valores = new int[100];
+            Random randNum = new Random();
+            int num;
+            string id = " ";
+
+            num = randNum.Next(1000, 9999);
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+
+                if (num != valores[i])
+                {
+                    id = "MP" + num;
+                    valores[i] = num;
+                }
+                else
+                {
+                    num = randNum.Next(1000, 9999);
+                    valores[i] = num;
+                    id = "MP" + num;
+                }
+            }
+
+            string nome;
+            do
+            {
+                Console.Write("Informe o nome (máximo 20 caracteres): ");
+                nome = Console.ReadLine();
+
+            } while (nome.Length > 20);
+
+            //leitura da data
+            bool deuCerto = false;
+            Console.Write("Informe data da última compra (dd/MM/yyyy): ");
+            DateTime ucompra;// = DateTime.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            do
+            {
+                if (DateTime.TryParse(Console.ReadLine(), out ucompra))
+                {
+                    deuCerto = true;
+                }
+            } while (!deuCerto);
+
+            //guardando data local
+            DateTime dcadastro = DateTime.Now;
+            Console.Write("Data do cadastro: " + dcadastro + "\n");
+
+            //leitura e teste de situação
+            char situacao;
+            do
+            {
+                Console.Write("Situação (A ou I): ");
+                situacao = char.Parse(Console.ReadLine().ToUpper());
+            } while (situacao != 'A' && situacao != 'I');
+
+            //instancia de obj materia prima
+            MPrima materiaPrima = new MPrima(id, nome, ucompra, dcadastro, situacao);
+
+            return materiaPrima;
+        }
+    }
+}
---
+++ SysBil/Model/MPrima.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class MPrima
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Ucompra { get; set; }
+        public DateTime Dcadastro { get; set; }
+        public char Situacao { get; set; }
+
+        public MPrima() { }
+        public MPrima(string id, string nome, DateTime ucompra, DateTime dcadastro, char situacao)
+        {
+            Id = id;
+            Nome = nome;
+            Ucompra = ucompra;
+            Dcadastro = dcadastro;
+            Situacao = situacao;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}\nNome: {Nome}\nData Compra: {Ucompra}\nData Cadastro: {Dcadastro}\nSituação: {Situacao}";
+        }
+    }
+}
