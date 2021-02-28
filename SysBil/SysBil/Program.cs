4152fec404213138b7f6609acc8d44600aa2f45d
Bruno Bronze <bruno_oliveirabronze@hotmail.com>
2021-02-28 13:19:42 -0300

    Update Program.cs

--- SysBil/SysBil/Program.cs
+++ SysBil/SysBil/Program.cs
@@ -1,17 +1,49 @@
-﻿using System;
+using System;
+using Controllers;
+using Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Controllers;
 
 namespace SysBil
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			ControllersArquivoBloqueados.Menu();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu = "1 - Menu fornecedor\n" +
+                          "2 - Menu fornecedor bloqueado\n" +
+                          "3 - Menu compra\n" +
+                          "4 - Encerrar\n";
+            string op;
+            do
+            {
+                Console.WriteLine(menu);
+                Console.Write(">>>");
+                op = Console.ReadLine();
+
+
+                switch (op)
+                {
+                    case "1":
+                        ControllersFornecedor.Menu();
+                        break;
+
+                    case "2":
+                        ControllersArquivoBloqueados.Menu();
+                        break;
+
+                    case "3":
+                        ControllersCompra.Menu();
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.Write(">>>");
+                op = Console.ReadLine();
+            } while (op != "4");
+        }
+    }
 }
