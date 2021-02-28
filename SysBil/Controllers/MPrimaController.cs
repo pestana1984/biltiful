df1525a7d23553260ff68f738110193d9549eeb5
ThiagoMacola <thiago_macola@hotmail.com>
2021-02-28 15:51:32 -0300

    Add metodo Localizar para editar

--- SysBil/Controllers/MPrimaController.cs
+++ SysBil/Controllers/MPrimaController.cs
@@ -7,74 +7,73 @@ using System.Threading.Tasks;
 
 namespace Controllers
 {
-    public class MPrimaController
-    {
-        public static MPrima Cadastrar()
-        {
-            //Gerador de id para matéria prima
-            //MP1234 -> formato | guardando resultados
-            int[] valores = new int[100];
-            Random randNum = new Random();
-            int num;
-            string id = " ";
+	public class MPrimaController
+	{
+		public static MPrima Cadastrar()
+		{
+			//Gerador de id para matéria prima
+			//MP1234 -> formato | guardando resultados
+			int[] valores = new int[100];
+			Random randNum = new Random();
+			int num;
+			string id = " ";
 
-            num = randNum.Next(1000, 9999);
+			num = randNum.Next(1000, 9999);
 
-            for (int i = 0; i < valores.Length; i++)
-            {
+			for (int i = 0; i < valores.Length; i++)
+			{
 
-                if (num != valores[i])
-                {
-                    id = "MP" + num;
-                    valores[i] = num;
-                }
-                else
-                {
-                    num = randNum.Next(1000, 9999);
-                    valores[i] = num;
-                    id = "MP" + num;
-                }
-            }
+				if (num != valores[i])
+				{
+					id = "MP" + num;
+					valores[i] = num;
+				}
+				else
+				{
+					num = randNum.Next(1000, 9999);
+					valores[i] = num;
+					id = "MP" + num;
+				}
+			}
 
-            string nome;
-            do
-            {
-                Console.Write("Informe o nome (máximo 20 caracteres): ");
-                nome = Console.ReadLine();
+			string nome;
+			do
+			{
+				Console.Write("Informe o nome (máximo 20 caracteres): ");
+				nome = Console.ReadLine();
 
-            } while (nome.Length > 20);
+			} while (nome.Length > 20);
 
-            //leitura da data
-            bool deuCerto = false;
-            Console.Write("Informe data da última compra (dd/MM/yyyy): ");
-            DateTime ucompra;// = DateTime.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            do
-            {
-                if (DateTime.TryParse(Console.ReadLine(), out ucompra))
-                {
-                    deuCerto = true;
-                }
-            } while (!deuCerto);
+			//leitura da data
+			bool deuCerto = false;
+			Console.Write("Informe data da última compra (dd/MM/yyyy): ");
+			DateTime ucompra;// = DateTime.TryParse(Console.ReadLine(), CultureInfo.InvariantCulture);
+			do
+			{
+				if (DateTime.TryParse(Console.ReadLine(), out ucompra))
+				{
+					deuCerto = true;
+				}
+			} while (!deuCerto);
 
-            //guardando data local
-            DateTime dcadastro = DateTime.Now;
-            Console.Write("Data do cadastro: " + dcadastro + "\n");
+			//guardando data local
+			DateTime dcadastro = DateTime.Now;
+			Console.Write("Data do cadastro: " + dcadastro + "\n");
 
-            //leitura e teste de situação
-            char situacao;
-            do
-            {
-                Console.Write("Situação (A ou I): ");
-                situacao = char.Parse(Console.ReadLine().ToUpper());
-            } while (situacao != 'A' && situacao != 'I');
+			//leitura e teste de situação
+			char situacao;
+			do
+			{
+				Console.Write("Situação (A ou I): ");
+				situacao = char.Parse(Console.ReadLine().ToUpper());
+			} while (situacao != 'A' && situacao != 'I');
 
-            //instancia de obj materia prima
-            MPrima materiaPrima = new MPrima(id, nome, ucompra, dcadastro, situacao);
+			//instancia de obj materia prima
+			MPrima materiaPrima = new MPrima(id, nome, ucompra, dcadastro, situacao);
 
-            return materiaPrima;
-        }
-        
-        		public static void LocalizarMPrima()
+			return materiaPrima;
+		}
+		public static void LocalizarMPrima()
 		{
 			bool itemEncontrado = false;
 
@@ -94,12 +93,36 @@ namespace Controllers
 			}
 
 			if (!itemEncontrado)
-				Console.WriteLine("Materia prima não cadastrada");
+				Console.WriteLine("Impossível localizar! Item não cadastrado.");
 		}
-
-		public static List<MPrima> ConverterParaLista() 
+		public static MPrima LocalizarParaEditarMP()
 		{
-			FileManipulator file = new FileManipulator() { Path = @"C:\git\biltiful\files", Name = "Materia.txt" };
+			bool itemEncontrado = false;
+
+			Console.Write("Informe o ID da materia prima para localizar: ");
+			string buscarId = Console.ReadLine();
+
+			List<MPrima> MPrimas = ConverterParaLista();
+			MPrima objmprima = new MPrima();
+			foreach (var mprima in MPrimas)
+			{
+				if (mprima.Id == buscarId)
+				{
+					itemEncontrado = true;
+					Console.WriteLine(mprima.ToString());
+					objmprima = mprima;
+					break;
+				}
+			}
+
+			if (!itemEncontrado)
+				Console.WriteLine("Impossível localizar! Item não cadastrado.");
+
+			return objmprima;
+		}
+		public static List<MPrima> ConverterParaLista()
+		{
+			FileManipulator file = new FileManipulator() { Path = @"C:\Users\Thiago\Desktop\PROJETO\biltiful\SysBil\files", Name = "Materia.txt" };
 			string[] mprimaArquivadas = FileManupulatorController.LerArquivo(file);
 
 			List<MPrima> MPrimas = new List<MPrima>();
@@ -128,5 +151,5 @@ namespace Controllers
 			}
 			return MPrimas;
 		}
-    }
+	}
 }
