e1f2c6ba319c6612f78d7703d9eab2f4c188b82c
Saulo Godoy Proetti <38708863+saulogp@users.noreply.github.com>
2021-02-28 22:28:53 -0300

    add file produção model

---
+++ SysBil/Model/Producao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Producao
+    {
+        public int Id { get; set; }
+        public DateTime DProducao { get; set; }
+        public string CBarras { get; set; }
+        public int Qtd { get; set; }
+        public string[] IdMP{ get; set; }
+        public double[] QtdMP { get; set; }
+
+        public Producao()
+        {
+
+        }
+
+        public Producao(int id, DateTime dp, string cb, int qtd, string[] idmp, double[] qtdmp)
+        {
+            Id = id;
+            DProducao = dp;
+            CBarras = cb;
+            Qtd = qtd;
+            IdMP = idmp;
+            QtdMP = qtdmp;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nData produção: {DProducao}\nCódigo de Barras: {CBarras}\nQuantidade: {Qtd}\nID Matéria Prima: {IdMP}\nQuantidade: {QtdMP}";
+        }
+    }
+}
